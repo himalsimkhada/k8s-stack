f72de6b0d329e4b52cda5106fe4189dac13aa416
Himal Simkhada <46737825+himalsimkhada@users.noreply.github.com>
2026-05-31 13:19:32 +0545

    Add Grafana admin and SMTP secret creation commands
    Added commands to create secrets for Grafana admin and SMTP configurations.

--- monitoring-stack/grafana/helm-command.sh
+++ monitoring-stack/grafana/helm-command.sh
@@ -1,3 +1,13 @@
+kubectl create secret generic grafana-admin-secret \
+  --from-literal=admin-user=admin \
+  --from-literal=admin-password='Devops@123' \
+  -n grafana
+
+kubectl create secret generic grafana-smtp-secret \
+  --from-literal=user='your-email@example.com' \
+  --from-literal=password='your-email-password-or-app-password' \
+  -n grafana
+
 helm repo add grafana-community https://grafana-community.github.io/helm-charts
 helm repo update
 
