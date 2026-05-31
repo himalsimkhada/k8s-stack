391c8df5faf77c730542306302eddf35a07a214c
Himal Simkhada <46737825+himalsimkhada@users.noreply.github.com>
2026-05-31 13:21:05 +0545

    Add namespace creation to helm upgrade command

--- monitoring-stack/grafana/helm-command.sh
+++ monitoring-stack/grafana/helm-command.sh
@@ -12,6 +12,6 @@ helm repo add grafana-community https://grafana-community.github.io/helm-charts
 helm repo update
 
 helm upgrade --install grafana grafana-community/grafana \
-  -n grafana -f values.yml
+  -n grafana --create-namespace -f values.yml
 
 helm uninstall grafana -n grafana
