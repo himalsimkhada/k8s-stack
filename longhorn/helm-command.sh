232b2fddd0b8dd3dafe8c9cfd440f6f8769ad94b
Himal Simkhada <46737825+himalsimkhada@users.noreply.github.com>
2026-05-31 11:54:15 +0545

    Change helm install to helm upgrade --install

--- longhorn/helm-command.sh
+++ longhorn/helm-command.sh
@@ -2,7 +2,7 @@ helm repo add longhorn https://charts.longhorn.io
 
 helm repo update
 
-helm install longhorn longhorn/longhorn \
+helm upgrade --install longhorn longhorn/longhorn \
   --namespace longhorn-system \
   --create-namespace \
   -f values.yml
